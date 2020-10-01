3ec102a7d9538fb2315ad66c94f37e8b5a28c212
cpcepa <poulocp@hotmail.com>
2020-10-01 17:58:46 +0800

    fix the module settings

---
+++ meta_argparser/__init__.py
@@ -0,0 +1,1 @@
+from .meta_argparser import parse_yaml
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cpcepa/algo_util",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(include=['meta_argparser','meta_argparser.*']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
